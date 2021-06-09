cb9c785c4189e67ec1db3dbc8f3b45081af4ce75
git-marci <marcello_mauro@hotmail.com>
2021-06-09 15:53:15 +0200

    Esercizio ArrayTest 09/06/2021 + metodo per ricavare array di interi in input

--- src/ires/corso/partone/ArrayTest.java
+++ src/ires/corso/partone/ArrayTest.java
@@ -1,9 +1,29 @@
 package ires.corso.partone;
 
+import java.util.Scanner;
+
 public class ArrayTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println("Inserisci un array (numeri separati da |, esempio: 1|2|3|...)");
+        System.out.print("==>");
+        Scanner in = new Scanner(System.in);
+        String nl = in.nextLine();
+        //String[] strArr = nl.split("\\|");
+        String[] strArr = nl.split(" ");
 
+        int[] intArr = new int[strArr.length];
+
+        for(int j = 0; j < strArr.length; j++) {
+            System.out.printf("Elemento %d-esimo dell'array ==> %s\n", j + 1, strArr[j]);
+            intArr[j] = Integer.parseInt(strArr[j]);
+        }
+
+        System.out.print("Dimmi l'elemento da cercare: ");
+        int j = in.nextInt();
+        int p = elemPositionInArray(intArr, j);
+        System.out.printf("L'elemento si trova in posizione %d\n", p);
     }
 
     // Scrivere un metodo java che accetta in ingresso un

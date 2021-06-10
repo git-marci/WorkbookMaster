9739c1ed39e1a2946e8d7b2b1eb1eac34787901f
git-marci <marcello_mauro@hotmail.com>
2021-06-10 17:56:43 +0200

    Esercizio sulla creazione degli oggetti

---
+++ src/ires/corso/parttwo/firstclasses/FirstClassTest.java
@@ -0,0 +1,20 @@
+package ires.corso.parttwo.firstclasses;
+
+public class FirstClassTest
+{
+    // Create 2 classi a vostro piacimento, ispirandovi agli oggetti che + vi piacciono.
+    //
+    // Includete:
+    // - almeno un campo int/long
+    // - almeno un campo Stringa
+    // - almeno un campo boolean
+    //
+    // TUTTE le variabili devono essere "private"!
+    //
+    // Aggiungete:
+    // - getter e setter per tutti i campi
+    // - un costruttore diverso da quello di default che accetta almeno 2 argomenti e li imposta come valore dei campi
+    // - un metodo "prettyPrint" che stampa in console le caratteristiche dell'istanza
+    //
+    // Fate un test creando almeno un'istanza di ciascuna classe
+}

a83ecdc4e403e72bdf2a927c76747cf480d02306
git-marci <marcello_mauro@hotmail.com>
2021-06-17 17:58:05 +0200

    Compito interfacce mostri

---
+++ src/ires/corso/parttwo/firstclasses/InterfaceTest.java
@@ -0,0 +1,6 @@
+package ires.corso.parttwo.firstclasses;
+
+public class InterfaceTest
+{
+    // Svolgere l'esercizio contenuto nel pdf: https://github.com/git-marci/Blackboard/blob/master/src/ires/corso/parttwo/J-12b.EsempioGiocoInterfacce.pdf
+}

1813399a91385a049ce37e85075a726ad6b30faf
git-marci <marcello_mauro@hotmail.com>
2021-06-24 17:05:10 +0200

    Upload esercizi JFC

---
+++ src/ires/corso/parttwo/collections/exercises.java
@@ -0,0 +1,53 @@
+package ires.corso.parttwo.collections;
+
+public class exercises
+{
+    // ArrayList
+    // Set
+    // Map
+
+    // 1. Studenti e corsi
+    //
+    //    Creare le seguenti classi:
+    //    - Student (nome, cognome, codiceStudente, tutti String)
+    //    - Course (titolo, descrizione, settore)
+    //    - Assignment (titolo, descrizione)
+    //
+    //    Create anche i dati sdi almeno:
+    //    - 3 studenti
+    //    - 3 corsi
+    //    - un corso ha 2 o + assignment, ciascun studente esegue tutti gli assignment ricevendo un voto da 1 a 10
+    //
+    //    Caricate tutti i dati in un'unica collection, organizzata in modo da poter contenere tutti i voti presi
+    //    da ciascuno studente.
+    //
+    //    Calcolate con questa struttura:
+    //    - la media dei voti di ciascun studente in ciascun corso (come è andato ciascun studente in un corso?)
+    //    - la media dei voti di tutti gli studenti in ciascun assignment (quanto è difficile per gli studenti
+    //      un certo assignment?)
+
+    // 2. Biblioteca
+    //
+    //    Creare le seguenti classi:
+    //    - Libro (autore, data pubblicazione, num. pagine, num. volumi, num. capitoli)
+    //    - Categoria (titolo, descrizione)
+    //    - Utente (nome, cognome, id)
+    //    - Prestito (Utente, 1 o + Libro)
+    //
+    //    Create almeno 8 libri diversi e 5 categorie diverse.
+    //    Almeno 4 libri devono essere associati a più di una categoria.
+    //    Rappresentate opportunamente l'associazione tra libri e categorie in una struttura (basata sulle Java
+    //    collections) che permetta di ricavare agevolmente a quali categorie appartiene un libro.
+    //
+    //    Create poi almeno 3 utenti diversi.
+    //    Per ciascun utente create almeno 2 prestiti, con 2 o + volumi appartenenti a categorie distinte.
+    //    Rappresentate opportunamente l'associazione tra utenti e prestiti tramite una struttura (basata sulle
+    //    Java collections) che permetta di ricavare agevolmente quali prestiti ha eseguito un utente.
+    //
+    //    Incrociate i dati delle due strutture per costruire una terza struttura (basata.....) che dica
+    //    quanti e quali prestiti sono stati eseguiti per ciascuna categoria (vogliamo cioè fare un'analisi
+    //    statistica di quali siano le categorie più gradite).
+    //
+    //    Tutte le classi possono essere pensate come semplici strutture dati: quindi campi private e final,
+    //    valori dei campi da passare nel costruttore, solo metodi "getter" per recupare i valori.
+}

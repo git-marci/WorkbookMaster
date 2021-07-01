bb00b1026c7f56c1ed4552d1eb807bcbb9e60d13
git-marci <marcello_mauro@hotmail.com>
2021-07-01 18:08:40 +0200

    Menu con classi

---
+++ src/ires/corso/partthree/LambdaAssignment.java
@@ -0,0 +1,20 @@
+package ires.corso.partthree;
+
+public class LambdaAssignment
+{
+    // 1. Data una lista di nomi:
+    //    - Agnese Stefano, Niki, Veronica, GabrieleM, Davide, GabrieleG, Alessandor, Albnerto, Fabiola, Alessio, Daniele
+    //    - ...partire con un ArrayList che li contiene;
+    //    - trasformarlo in stream
+    //    - filtrare (escludendoli) i nomi che hanno lunghezza < 5 lettere
+    //    - trasformare con UPPERCASE i rimanenti
+    //    - stamparli su schermo
+    //    ...concatenando il più possibile le varie operazioni
+
+    // 2. Fare un programma Java che:
+    //    - chiede all'utente una lista di numeri (separati da virgola) <== RIUSATE IL CODICE DEL SORTING GAME
+    //    - li trasforma in stream
+    //    - filtra i duplicati (HINT: guardate bene i metodi dell'interfaccia Stream...)
+    //    - eleva al quadrato i numeri filtrati
+    //    - calcola la media (HINT: operazione terminale chiamata "average")
+}
